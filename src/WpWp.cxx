5bcce9a112e519fdf1781e285dcef9b076259b7f
paktinat <saeid.paktinat@cern.ch>
2016-10-31 14:51:21 +0330

    some mistakes fixed for WpWp.cxx

--- src/WpWp.cxx
+++ src/WpWp.cxx
@@ -164,11 +164,11 @@ void LHEF::Loop()
     TLorentzVector nTau_1;
     TLorentzVector nTau_2;
 
- for(int ti =0 ; ti < wp_p -> getDoughters().size() ; ti++){
+ for(int ti =0 ; ti < wp_p -> getDaughters().size() ; ti++){
    TauolaParticle* t =  wp_p ->getDaughters()[ti] ;
   	if(t -> getPdgID() == 16 )
 	  nTau_1.SetXYZT( wpp_d2->getPx() , wpp_d2->getPy() , wpp_d2->getPz() , wpp_d2->getE() );
-      }
+      
 	if(t -> getPdgID() == -16 )
 	  nTau_2.SetXYZT( wpn_d2->getPx() , wpn_d2->getPy() , wpn_d2->getPz()  , wpn_d2->getE() );
       }
@@ -243,7 +243,7 @@ void LHEF::Loop()
       continue;
 
 
-    TLorentzVector met = ( -(nTau_1 + anTau_2 ))+(-(vtau_1 + vtau_2) );
+    TLorentzVector met = ( -(nTau_1 + nTau_2 ))+(-(vtau_1 + vtau_2) );
     TLorentzVector tauSystem = vtau_1 + vtau_2 ;
     double systemMass = tauSystem.M();
 
